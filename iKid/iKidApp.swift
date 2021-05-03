5da346e395e97f24384d7c09f9a9ac3613f929c3
Ryan Lee <ralee478.school@gmail.com>
2021-05-03 16:04:10 -0700

    Initial Commit

---
+++ iKid/iKidApp.swift
@@ -0,0 +1,17 @@
+//
+//  iKidApp.swift
+//  iKid
+//
+//  Created by Ryan Lee on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iKidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
